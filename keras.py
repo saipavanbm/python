e21b7ac7d5462f90753b1b4eb7d2dd0f1304baac
saipavanbm <47355967+saipavanbm@users.noreply.github.com>
2020-05-30 23:23:11 +0530

    Update keras.py

--- keras.py
+++ keras.py
@@ -3,6 +3,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.layers import BatchNormalization
 dataset=tensorflow.keras.datasets.mnist.load_data(path="mnist.npz")
 train,test=dataset
 x_train,y_train=train
@@ -12,11 +13,14 @@ x_train_1d = x_train.reshape(-1 , 28*28)
 x_test_1d = x_test.reshape(-1 , 28*28)
 x_train = x_train_1d.astype('float32')
 x_test = x_test_1d.astype('float32')
+x_train = x_train / 255.0
+x_test = x_test/ 255.0
 y_train_cat=to_categorical(y_train)
 y_test_cat=to_categorical(y_test)
 model=Sequential()
 model.add(Dense(units=512,input_dim=28*28,activation='relu'))
 model.add(Dense(units=256, activation='relu'))
+model.add(BatchNormalization())
 model.add(Dense(units=10, activation='softmax'))
 model.summary()
 model.compile(optimizer=Adam(), loss='categorical_crossentropy',metrics=['accuracy'])
